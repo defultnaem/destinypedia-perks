c7842560e075ca74831fb9e93afa8c9708dac840
jwjrowberry <jwjrowberry@gmail.com>
2023-07-14 19:09:11 +0100

    swapped hash for name in weapon file

--- GenerateWeaponList.py
+++ GenerateWeaponList.py
@@ -4,7 +4,7 @@ import os
 def GenerateWeaponList():
     file_name = "DestinyInventoryItemDefinition.json"
     weapon_list = []
-    hash_list = []
+    name_list = []
 
     with open(file_name, "r", encoding="utf-8") as f:
         my_list = json.load(f)
@@ -13,16 +13,16 @@ def GenerateWeaponList():
             if obj != None:
                 if obj["itemType"] == 3:
                     weapon_list.append(obj)
-                    hash_list.append(obj["hash"])
+                    name_list.append(obj["displayProperties"]["name"])
                     print(str(obj["hash"])+": "+obj["displayProperties"]["name"]+" added")
 
     new_file_name = "DestinyWeaponDefinition.json"
 
     f = open(new_file_name, "w")
-    hash_index = 0
+    name_index = 0
     for weapon in weapon_list:
-        f.write("'"+str(hash_list[hash_index])+"': "+str(weapon)+"\n")
-        hash_index = hash_index + 1
+        f.write('"'+str(name_list[name_index])+'": '+str(weapon)+"\n")
+        name_index = name_index + 1
     f.close()
     print("weapon file generated")
 
